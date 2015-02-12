7490bcb4ae1a3f3d7cda2f5959694441013739c4
Tim Martin <timothy.m.martin@gmail.com>
2015-02-11 18:47:27 -0800

    Added some semantic styling.

--- src/scripts/components/ReactAppApp.js
+++ src/scripts/components/ReactAppApp.js
@@ -6,31 +6,44 @@ var ReactTransitionGroup = React.addons.TransitionGroup;
 // Export React so the devtools can find it
 (window !== window.top ? window.top : window).React = React;
 
-// CSS
+// Style Components
 require('../../styles/normalize.css');
 require('../../styles/app.css');
+require('../../lib/react-semantify.min.js');
 
-var imageURL = require('../../images/yeoman.png');
+var Button = Semantify.Button;
 
-var Question = React.createClass({
+var TodoList = React.createClass({
   render: function() {
-    return (
-      <div>
-        <h2>{this.props.query}</h2>
-        <input type="text"></input>
-      </div>
-      );
+    var createItem = function(itemText) {
+      return <li className="item">{itemText}</li>;
+    };
+    return <ul className="examplelist">{this.props.items.map(createItem)}</ul>;
   }
 });
 
 var ReactAppApp = React.createClass({
+  getInitialState: function() {
+    return {items: [], text: ''};
+  },
+  onChange: function(e) {
+    this.setState({text: e.target.value});
+  },
+  handleSubmit: function(e) {
+    e.preventDefault();
+    var nextItems = this.state.items.concat([this.state.text]);
+    var nextText = '';
+    this.setState({items: nextItems, text: nextText});
+  },
   render: function() {
     return (
-      <div className='app'>
-        <ReactTransitionGroup transitionName="fade">
-          <img src={imageURL} />
-          <Question query={'What is your name?'}></Question>
-        </ReactTransitionGroup>
+      <div>
+        <h3>To-Do List</h3>
+        <TodoList items={this.state.items} />
+        <form onSubmit={this.handleSubmit}>
+          <input className="exampleinput" onChange={this.onChange} value={this.state.text} />
+          <Button className="examplelabel">{'Add Task'}</Button>
+        </form>
       </div>
     );
   }
